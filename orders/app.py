edf0903292bb24736b47e664783ea8e8197c17e8
Yan Max Pinheiro <yanmaxsette@gmail.com>
2024-01-24 16:01:39 -0300

    Removed AMaaS

--- orders/app.py
+++ orders/app.py
@@ -20,27 +20,6 @@ def allowed_file(filename):
     # Allow all file types
     return True
 
-def scan_uploaded_file(file_path, handle):
-    try:
-        logging.info(f"Scanning file at path: {file_path}")
-        logging.info(f"Handle type: {type(handle)}")
-
-        if not isinstance(file_path, (str, bytes, os.PathLike)):
-            raise TypeError("file_path must be a string, bytes, or os.PathLike object")
-
-        result = amaas.grpc.scan_file(handle, file_path)
-        
-        # Log the scan result for debugging
-        logging.info(f"Scan result for {file_path}: {result}")
-
-        return result
-    except TypeError as te:
-        logging.error(f"TypeError during scanning: {te}")
-        return None
-    except Exception as e:
-        logging.error(f"Error during scanning: {e}")
-        return None
-
 def upload_to_s3(file_path, bucket_name, object_name=None, content_disposition=None):
     if object_name is None:
         object_name = file_path.split("/")[-1]
@@ -91,40 +70,13 @@ def upload_file():
 
         logging.info(f"File saved at {file_path}")
 
-        # Initialize the AMAAS handle
-        amaas_base_url = "antimalware.us-1.cloudone.trendmicro.com:443"
-        api_key = os.environ.get('V1_API_KEY').strip('"')
-        api_key = api_key.replace("ApiKey ", '')
-
-        handle = amaas.grpc.init(amaas_base_url, api_key, True)
-
-        # Scan the file
-        scan_result = scan_uploaded_file(file_path, handle)
-
-        if scan_result is not None:
-            scan_result_dict = json.loads(scan_result)
-            scan_result_code = scan_result_dict.get('scanResult', -1)
-
-            logging.info(f"Scan Result Code {scan_result_code}")
-            
-            if scan_result_code == 1:
-                amaas.grpc.quit(handle)
-                logging.warning("File contains malware. Uploading is not allowed.")
-                return jsonify({'scan_result_code': scan_result_code, 'scan_results': scan_result_dict}), 403
-        else:
-            amaas.grpc.quit(handle)
-            logging.error("File not scanned. Check the logs for details.")
-            return jsonify({'error': "File not scanned. Check the logs"}), 500
-
-        amaas.grpc.quit(handle)
-
         # Upload to S3
         bucket_name = os.environ.get('s3_bucket_name')
 
         if upload_to_s3(file_path, bucket_name, None, 'inline'):
-            return jsonify({'message': "File uploaded successfully.", 'uploaded': True, 'scan_result_code': scan_result_code, 'scan_results': scan_result_dict})
+            return jsonify({'message': "File uploaded successfully.", 'uploaded': True})
         else:
-            return jsonify({'error': "Error uploading the file.", 'uploaded': False, 'scan_result_code': scan_result_code, 'scan_results': scan_result_dict}), 500
+            return jsonify({'error': "Error uploading the file.", 'uploaded': False}), 500
 
     return jsonify({'error': "File not allowed"}), 400
 
